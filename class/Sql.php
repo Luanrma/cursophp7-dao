c1dcd76fa2b2e96af53d5a2c50082822f158fe07
Luanrma <luanrma@gmail.com>
2019-04-09 22:01:25 -0300

    métodos de lista

--- class/Sql.php
+++ class/Sql.php
@@ -6,14 +6,12 @@
 			$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
 		}
 		
-		// setParams será instanciada quando houver um INSERT com mais que uma Coluna na Tabela.
 		private function setParams($statement, $parameters = array()) {
 			foreach ($parameters as $key => $value) {
 				$this->setParam($statement, $key, $value);
 			}
 		}
 		
-		// setParams será instanciada quando houver um INSERT com apenas uma Coluna na Tabela.
 		private function setParam($statement, $key, $value) {
 			// $key = nome da coluna;
 			$statement->bindParam($key, $value);
--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
 	require_once("config.php");
 
-	$root - new Usuario;
+	//Carrega um usuário
+	//$root = new Usuario();
+	//$root->loadbyId(3);
+	//echo $root;
 	
-	$root->loadbyId(3);
+	//Carrega uma lista de usuários
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
 
-	echo $root;
+	//Carrega uma lista de usuários buscando pelo login
+	//$search = Usuario::search("o");
+	//echo json_encode($search);
 
+	//Carrega um usuário usando o login e a senha
+	//$usuario = new Usuario();
+	//$usuario->login("Jose","12345");
+	//echo $usuario;
+
+	$aluno = new Usuario();
+	$aluno->setDeslogin("aluno");
+	$aluno->setDessenha("@lun0");
+	$aluno->insert();
+	echo $aluno;
 ?>
